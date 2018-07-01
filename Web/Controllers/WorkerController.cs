eec044def72a47a824ebdc97bb4318b7017a3ba6
Joel R. Paderagao <joel.paderagao@outlook.com>
2018-07-01 16:29:54 +0800

    completed singleton pattern using logger project

---
+++ Web/Controllers/WorkerController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+using Logger;
+namespace Web.Controllers
+{
+    public class WorkerController : Controller
+    {
+        private EmployeeEntities db = new EmployeeEntities();
+        ILog _ILog;
+
+
+        public WorkerController()
+        {
+            _ILog = Log.GetInstance;
+        }
+        
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            _ILog.LogException(filterContext.Exception.ToString());
+            filterContext.ExceptionHandled = true;
+            this.View("Error").ExecuteResult(this.ControllerContext);
+
+        }
+
+        // GET: Worker
+        public ActionResult Index()
+        {
+            return View(db.Workers.ToList());
+        }
+
+        // GET: Worker/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Worker worker = db.Workers.Find(id);
+            if (worker == null)
+            {
+                return HttpNotFound();
+            }
+            return View(worker);
+        }
+
+        // GET: Worker/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Worker/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,Position,Department")] Worker worker)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Workers.Add(worker);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(worker);
+        }
+
+        // GET: Worker/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Worker worker = db.Workers.Find(id);
+            if (worker == null)
+            {
+                return HttpNotFound();
+            }
+            return View(worker);
+        }
+
+        // POST: Worker/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Position,Department")] Worker worker)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(worker).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(worker);
+        }
+
+        // GET: Worker/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Worker worker = db.Workers.Find(id);
+            if (worker == null)
+            {
+                return HttpNotFound();
+            }
+            return View(worker);
+        }
+
+        // POST: Worker/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Worker worker = db.Workers.Find(id);
+            db.Workers.Remove(worker);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
